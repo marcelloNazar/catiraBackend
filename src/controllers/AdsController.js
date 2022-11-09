b2f3cb474ff0e31395b3067c17a5c8259b10430c
Marcello Nazar <marcellonazarb@hotmail.com>
2022-11-09 19:23:09 -0300

    Add AdsController

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,14 +1,15 @@
-const uuid = require("uuid");
+const { v4: uuid } = require("uuid");
 const jimp = require("jimp");
 
 const Category = require("../models/Category");
 const User = require("../models/User");
 const Ad = require("../models/Ad");
+const State = require("../models/State");
 
 const addImage = async (buffer) => {
   let newName = `${uuid()}.jpg`;
   let tmpImg = await jimp.read(buffer);
-  tmpImg.cover(500, 500).quality(80).write(`./public/media/${nwName}`);
+  tmpImg.cover(500, 500).quality(80).write(`./public/media/${newName}`);
   return newName;
 };
 
@@ -28,10 +29,10 @@ module.exports = {
     res.json({ categories });
   },
   addAction: async (req, res) => {
-    let { title, price, priceng, desc, cat, token } = req.body;
+    let { title, price, priceneg, desc, cat, token } = req.body;
     const user = await User.findOne({ token }).exec();
 
-    if (!tittle || !cat) {
+    if (!title || !cat) {
       res.json({ error: "Titulo e/ou categoria não foram preechidos" });
       return;
     }
@@ -57,14 +58,134 @@ module.exports = {
 
     if (req.files && req.files.img) {
       if (req.files.img.length == undefined) {
+        if (
+          ["image/jpeg", "image/jpg", "image/png"].includes(
+            req.files.img.mimetype
+          )
+        ) {
+          let url = await addImage(req.files.img.data);
+          newAd.images.push({
+            url,
+            default: false,
+          });
+        }
       } else {
+        for (leti = 0; i < req.files.img.length; i++) {
+          if (
+            ["image/jpeg", "image/jpg", "image/png"].includes(
+              req.files.img[i].mimetype
+            )
+          ) {
+            let url = await addImage(req.files.img[i].data);
+            newAd.images.push({
+              url,
+              default: false,
+            });
+          }
+        }
+      }
+      if (newAd.images.length > 0) {
+        newAd.images[0].default = true;
       }
     }
 
     const info = await newAd.save();
     res.json({ id: info._id });
   },
-  getList: async (req, res) => {},
-  getItem: async (req, res) => {},
+  getList: async (req, res) => {
+    let { sort = "asc", offset = 0, limit = 8, q, cat, state } = req.query;
+    let filters = { status: true };
+    let total = 0;
+
+    if (q) {
+      filters.title = { $regex: q, $options: "i" };
+    }
+    if (cat) {
+      const c = await Category.findOne({ slug: cat }).exec();
+      if (c) {
+        filters.category = c._id.toString();
+      }
+    }
+    if (state) {
+      const s = await State.findOne({ name: state.toUpperCase() }).exec();
+      if (s) {
+        filters.state = s._id.toString();
+      }
+    }
+
+    const adsTotal = await Ad.find(filters).exec();
+    total = adsTotal.length;
+
+    const adsData = await Ad.find(filters)
+      .sort({ dateCreated: (sort = "desc" ? -1 : 1) })
+      .skip(parseInt(offset))
+      .limit(parseInt(limit))
+      .exec();
+    let ads = [];
+    for (let i in adsData) {
+      let image;
+      let defaultImg = adsData[i].images.find((e) => e.default);
+      if (defaultImg) {
+        image = `${process.env.BASE}/media/${defaultImg.url}`;
+      } else {
+        image = `${process.env.BASE}/media/default.jpg`;
+      }
+
+      ads.push({
+        id: adsData[i]._id,
+        title: adsData[i].title,
+        price: adsData[i].price,
+        priceNegotiable: adsData[i].priceNegotiable,
+        image,
+      });
+    }
+    res.json({ ads, total });
+  },
+  getItem: async (req, res) => {
+    let { id, other = null } = req.query;
+
+    if (!id) {
+      res.json({ error: "Sem produto" });
+      return;
+    }
+
+    if (id.length < 12) {
+      res.json({ error: "ID invalido" });
+      return;
+    }
+    const ad = await Ad.findById(id);
+    if (!ad) {
+      res.json({ error: "Produto inexistente" });
+    }
+
+    ad.views++;
+    await ad.save();
+
+    let images = [];
+    for (let i in ad.images) {
+      images.push(`${process.env.BASE}/media/${ad.images[i].url}`);
+    }
+
+    let category = await Category.findById(ad.category).exec();
+    let userInfo = await User.findById(ad.idUser).exec();
+    let stateInfo = await StateModel.findById(ad.state).exec();
+
+    res.json({
+      id: Ad._id,
+      title: Ad.title,
+      price: Ad.price,
+      priceNegotiable: Ad.priceNegotiable,
+      description: Ad.description,
+      dateCreated: Ad.dateCreated,
+      views: Ad.views,
+      images,
+      category,
+      userInfo: {
+        name: userInfo.name,
+        email: userInfo.email,
+      },
+      stateName: stateInfo.name,
+    });
+  },
   editAction: async (req, res) => {},
 };
